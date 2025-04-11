39b511e6e0a064a54dfd2c90258e141f62b991fa
vivek <you@example.com>
2025-04-11 09:33:21 +0530

    model change

--- src/models/UserModel.js
+++ src/models/UserModel.js
@@ -1,33 +1,38 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const userSchema = new Schema({
-  firstName: {
-    type: String,
+const userSchema = new Schema(
+  {
+    firstName: {
+      type: String,
+    },
+    lastName: {
+      type: String,
+    },
+    age: {
+      type: Number,
+    },
+    status: {
+      tpye: Boolean,
+    },
+    roleId: {
+      type: Schema.Types.ObjectId,
+      ref: "roles",
+    },
+    email: {
+      type: String,
+      unique: true,
+    },
+    password: {
+      type: String,
+    },
+    photoUrl: {
+      type: String,
+    },
   },
-  lastName: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  status: {
-    tpye: Boolean,
-  },
-  roleId: {
-    type: Schema.Types.ObjectId,
-    ref: "roles",
-  },
-  email: {
-    type: String,
-    unique: true,
-  },
-  password: {
-    type: String,
-  },
-  photoUrl: {
-    type: String,
-  },
-});
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("users", userSchema);
